4fd9eaa94f296864034b18486805db708f6a9677
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-07-12 10:49:28 -0700

    fixed!

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   'use strict';
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello, World Domination\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Hello beer.js!!</h1>');
 }).listen(port);
