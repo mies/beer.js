e7672d89dac113d315729961f8b8f4d4f08b5123
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-07-26 19:20:43 -0700

    removed comments

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-/*
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-    'use strict';
-    response.send('Hello World Domination!!');
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-    'use strict';
-    console.log("Listening on " + port);
-});
-*/
-
 var http = require('http');
 var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
